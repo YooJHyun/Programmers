3f75cc6ab3f730404cbeb19a3837da4a87937bc8
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-04-20 20:58:32 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+        // 전제가 num1에서 num2를 뺀 값을 구하므로 num1-num2의 값을 반환한다
+    }
+}
