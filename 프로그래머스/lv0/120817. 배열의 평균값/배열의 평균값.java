a596d7dd24018a2ecc5857fb1204c47863e642de
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-05-06 06:19:56 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer;
+        double tmp = 0;
+        for (int i = 0; i< numbers.length; i++) {
+            tmp += numbers[i];
+        }   // 배열의 0번째부터 길이까지 tmp에 합해주고
+        answer = (tmp / numbers.length);
+        // 배열 속 수를 더한 tmp에서 전체 길이로 나누어 평균값을 구한다 
+        return answer;
+    }
+}
