4645a755069137841a3b023885401407b4bb2526
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-05-08 21:46:12 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        answer = my_string.substring(0, n);
+        // my_string에서 n까지의 글자를 가져온다
+        return answer;
+    }
+}
