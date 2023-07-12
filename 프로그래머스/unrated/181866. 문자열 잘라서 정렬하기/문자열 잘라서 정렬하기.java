4226e758d76fea25ce3a2ba235b4b6182b84e28c
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-07-12 21:37:42 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 12.38 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String myString) {
+        String[] arr = myString.split("x");
+        // myString을 x를 기준으로 배열로 변환한다
+		Arrays.sort(arr);
+        // 배열을 정렬한다
+		List<String> list = new ArrayList<String>();
+		
+		for (String str : arr) {
+			if (!str.isEmpty()) {
+				list.add(str);
+			}
+	    } // 배열의 원소가 비어있지 않다면 list에 추가한다
+
+		String answer[] = list.toArray(new String[list.size()]);
+        // list를 배열로 재 변환한다
+        return answer;
+    }
+}
