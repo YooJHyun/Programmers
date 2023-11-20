735d3cd650023347d1f6b615f37b6c33a283068a
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-11-20 23:17:28 +0900

    [level unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] num_list, int n) {
         int answer = 0;
-        for(int num : num_list) {
-            if(num == n) {
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] == n) {
                 answer = 1;
-            }
+            } // num_list의 원소 중 n과 동일한 원소가 있을 경우 answer에 1을 대입한다
         }
         return answer;
     }
