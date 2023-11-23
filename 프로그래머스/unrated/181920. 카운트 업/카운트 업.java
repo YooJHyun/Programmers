a2ad9098454444b594c648cab5a071a216f16e2a
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-11-23 23:44:29 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,10 +1,13 @@
 class Solution {
-    public int[] solution(int start, int end) {
-        int num = end - start + 1;
-        int[] answer = new int[num];
-        for (int i = 0; i < num; i++) {
-            answer[i] = start + i;
-        }
+    public int[] solution(int start_num, int end_num) {
+		int[] answer = new int[end_num - start_num + 1];
+        // answer의 길이를 정한다
+		int j = 0;
+        
+		for(int i = start_num; i <= end_num; i++) {
+			answer[j] += i;
+			j++;
+		} // answer에 시작 숫자부터 종료 숫자까지 넣어준다
         return answer;
     }
 }
