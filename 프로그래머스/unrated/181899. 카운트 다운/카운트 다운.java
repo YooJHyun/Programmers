cac52d1612e176a691a61aed13ba5c7b61dd0707
YJH <130978507+YooJHyun@users.noreply.github.com>
2023-11-25 20:09:15 +0900

    [level unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,12 +1,6 @@
 class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num - end_num + 1];
-		// answer의 길이는 start_num와 end_num의 차이에 1을 더한다
-        int j = 0;
-		for(int i = start_num; i >= end_num; i--) {
-			answer[j] += i;
-			j++;
-		} // answer에 차례대로 내림차순으로 넣어준다
+    public int[] solution(int start, int end_num) {
+        int[] answer = {};
         return answer;
     }
 }
