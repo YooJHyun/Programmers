9474e51a08ad30be65cac2ba9a89f673259883a1
YJH <130978507+YooJHyun@users.noreply.github.com>
2024-08-14 23:50:19 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 31.47 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,13 @@
+function solution(arr, k) {
+    var answer = new Array(arr.length);
+    // answer를 arr와 같은 길이로 선언한다
+    for (let i = 0; i < arr.length; i++) {
+        if (k % 2 !== 0) {
+            answer[i] = arr[i] * k; 
+        }   // k가 홀수라면 모든 원소에 k를 곱해주고
+        else {
+            answer[i] = arr[i] + k;
+        }   // k가 짝수라면 모든 원소에 k를 더해준다
+    }
+    return answer;
+}
