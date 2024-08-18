64c72de8e78e9134c1b0300b928f64039e483f12
YJH <130978507+YooJHyun@users.noreply.github.com>
2024-08-18 22:27:17 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 p와 y의 개수 - 12916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=javascript) 
 
 ### 성능 요약
 
-메모리: 71.4 MB, 시간: 0.15 ms
+메모리: 33.5 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 23:59:25
+2024년 08월 18일 22:27:14
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,24 @@
+function solution(s){
+    var answer = true;
+
+    let p = 0;
+    let y = 0;
+
+    s = s.toLowerCase();
+    // s를 모두 소문자로 변환한다
+
+    for (let i = 0; i < s.length; i++) {
+        if (s.charAt(i) === 'p') {
+            p++;
+            // s의 i번째가 p일시 p를 카운트한다
+        } else if (s.charAt(i) === 'y') {
+            y++;
+            // s의 i번째가 y일시 y를 카운트한다
+        }
+    }
+    
+    if (p !== y) answer = false;
+    // 만약 p와 y의 카운트값이 다르다면 answer는 false가 된다
+        
+    return answer;
+}
